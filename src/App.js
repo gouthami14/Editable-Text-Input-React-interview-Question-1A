26ecb66851b2546d890a4339012ccc65b319ad14
gouthami14 <gouthamivodnala14@gmail.com>
2023-08-12 04:41:08 +0000

    Editable Text Input

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import './App.css'
+import Container from './components/Container'
+
+// Replace your code here
+const App = () => <Container />
+
+export default App
---
+++ src/components/Container/styledComponents.js
@@ -0,0 +1,55 @@
+import styled from 'styled-components'
+
+export const AppContainer = styled.div`
+  background-color: #f5d0fe;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+
+export const CardContainer = styled.div`
+  background-color: #fff;
+  padding: 30px;
+  height: 200px;
+  border-radius: 10px;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+
+export const Heading = styled.h1`
+  font-size: 20px;
+  font-family: 'Roboto';
+`
+
+export const InputContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`
+export const Paragraph = styled.p`
+  font-size: 15px;
+  font-family: 'Roboto';
+  padding-right: 10px;
+`
+export const Input = styled.input`
+  height: 30px;
+  width: 250px;
+  outline: none;
+  margin-right: 20px;
+  padding: 10px;
+`
+export const Button = styled.button`
+  height: 30px;
+  width: 55px;
+  cursor: pointer;
+  background-color: #d946ef;
+  color: #fff;
+  border-width: 0px;
+  font-size: 13px;
+  font-family: 'Roboto';
+`
